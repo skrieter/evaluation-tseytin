b797cf6e11eae3e1cacd7f3747a7c4f1ad97fd26
Elias Kuiter <info@elias-kuiter.de>
2021-08-24 09:51:56 +0200

    more models

--- src/main/java/org/spldev/evaluation/tseytin/TseytinEvaluator.java
+++ src/main/java/org/spldev/evaluation/tseytin/TseytinEvaluator.java
@@ -1,25 +1,3 @@
-/* -----------------------------------------------------------------------------
- * Evaluation-PC-Sampling - Program for the evaluation of PC-Sampling.
- * Copyright (C) 2021  Sebastian Krieter
- * 
- * This file is part of Evaluation-PC-Sampling.
- * 
- * Evaluation-PC-Sampling is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License,
- * or (at your option) any later version.
- * 
- * Evaluation-PC-Sampling is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with Evaluation-PC-Sampling.  If not, see <https://www.gnu.org/licenses/>.
- * 
- * See <https://github.com/skrieter/evaluation-pc-sampling> for further information.
- * -----------------------------------------------------------------------------
- */
 package org.spldev.evaluation.tseytin;
 
 import org.spldev.evaluation.Evaluator;
@@ -33,14 +11,10 @@ import org.spldev.formula.expression.Formulas;
 import org.spldev.formula.expression.io.XmlFeatureModelFormat;
 import org.spldev.formula.expression.transform.NFTransformer;
 import org.spldev.formula.expression.transform.TseytinTransformer;
-import org.spldev.formula.io.DIMACSFormatCNF;
-import org.spldev.util.io.FileHandler;
 import org.spldev.util.io.csv.CSVWriter;
 import org.spldev.util.io.format.FormatSupplier;
-import org.spldev.util.logging.Logger;
 import org.spldev.util.tree.Trees;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.function.Function;
 
@@ -55,7 +29,8 @@ public class TseytinEvaluator extends Evaluator {
 	@Override
 	protected void addCSVWriters() {
 		super.addCSVWriters();
-		writer = addCSVWriter("evaluation.csv", Arrays.asList("System", "Mode", "Iteration", "Simplification Time", "Transform Time", "Convert Time", "Analysis Time", "Variables", "Clauses", "Valid"));
+		writer = addCSVWriter("evaluation.csv",
+				Arrays.asList("System", "Mode", "Iteration", "Simplification Time", "Transform Time", "Convert Time", "Analysis Time", "Variables", "Clauses"));
 	}
 
 	@Override
@@ -69,7 +44,7 @@ public class TseytinEvaluator extends Evaluator {
 			ModelReader<Formula> fmReader = new ModelReader<>();
 			fmReader.setPathToFiles(config.modelPath);
 			fmReader.setFormatSupplier(FormatSupplier.of(new XmlFeatureModelFormat()));
-			for (int i = 0; i < config.systemIterations.getValue(); i++) {
+			for (int i = 0; i < config.systemIterations.getValue(); i++) { // jvm warmup? better call the transformation in this process or start another process? (memory leaks?)
 				Formula formula = fmReader.read(systemName).orElseThrow(p -> new RuntimeException("no feature model"));
 				transformToCNF(systemName, formula, i, TseytinEvaluator::toDistribCNF, "distrib"); // clausal CNF?
 				formula = fmReader.read(systemName).orElseThrow(p -> new RuntimeException("no feature model")); // optimize this?
@@ -89,32 +64,31 @@ public class TseytinEvaluator extends Evaluator {
 			long localTime = System.nanoTime();
 			formula = NFTransformer.simplifyForNF(formula);
 			long timeNeeded = System.nanoTime() - localTime;
-			writer.addValue(timeNeeded);
+			writer.addValue(String.format("%.1f", timeNeeded / 1000_000.0)); // formats sometimes in scientific notation?
 
 			localTime = System.nanoTime();
 			formula = transformer.apply(formula);
 			timeNeeded = System.nanoTime() - localTime;
-			writer.addValue(timeNeeded);
+			writer.addValue(String.format("%.1f", timeNeeded / 1000_000.0));
 
 			localTime = System.nanoTime();
 			CNF cnf = Clauses.convertToCNF(formula);
 			timeNeeded = System.nanoTime() - localTime;
-			writer.addValue(timeNeeded);
+			writer.addValue(String.format("%.1f", timeNeeded / 1000_000.0));
 
 			localTime = System.nanoTime();
-			boolean isValid = new HasSolutionAnalysis().getResult(new ModelRepresentation(formula)).get();
+			new HasSolutionAnalysis().getResult(new ModelRepresentation(formula)).get();
 			timeNeeded = System.nanoTime() - localTime;
-			writer.addValue(timeNeeded);
+			writer.addValue(String.format("%.1f", timeNeeded / 1000_000.0));
 
 			writer.addValue(cnf.getVariables().size());
 			writer.addValue(cnf.getClauses().size());
-			writer.addValue(isValid);
-			FileHandler.save(cnf,
-					config.csvPath.resolve(systemName + "-" + i + "-" + transformerName + ".dimacs"),
-					new DIMACSFormatCNF());
-		} catch (final IOException e) {
-			writer.removeLastLine();
-			Logger.logError(e);
+//			FileHandler.save(cnf,
+//					config.csvPath.resolve(systemName + "-" + i + "-" + transformerName + ".dimacs"),
+//					new DIMACSFormatCNF());
+//		} catch (final IOException e) {
+//			writer.removeLastLine();
+//			Logger.logError(e);
 		} finally {
 			writer.flush();
 		}
@@ -125,6 +99,6 @@ public class TseytinEvaluator extends Evaluator {
 	}
 
 	public static Formula toTseytinCNF(Formula formula) {
-		return Trees.traverse(formula, new TseytinTransformer()).get(); // this has side effects?
+		return Trees.traverse(formula, new TseytinTransformer()).get(); // this has side effects on the formula? why not TreeVisitor<Expression>?
 	}
 }
