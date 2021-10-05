7961b38f1df4f3df21d9dc2d2c061b0cfeb0b351
Elias Kuiter <info@elias-kuiter.de>
2021-10-05 11:32:45 +0200

    Updated evaluation

--- config/evaluation.R
+++ config/evaluation.R
@@ -1,20 +1,39 @@
 library(readr)
 library(tidyverse)
-#systems <- read_delim("systems.csv",delim = ";", escape_double = FALSE, trim_ws = TRUE)
-evaluation <- read_delim("evaluation.csv",delim = ";", escape_double = FALSE, trim_ws = TRUE)
+library('plot.matrix')
+library(viridis)
+graphics.off()
 
-system_ids <- function(system) {
-  return(with(systems, which(startsWith(System, "busybox/1_9_1.kconfigreader.model"))))
+max_num = list(0,10,100,1000,10000,2147483647)
+max_len = list(0,2,4,8,16,32,64,128,2147483647)
+systems <- read_delim("systems.csv", delim = ";", escape_double = FALSE, trim_ws = TRUE) # read systems
+dd <- read_delim("evaluation.csv", delim = ";", escape_double = FALSE, trim_ws = TRUE) # read actual data
+dd <- aggregate(. ~ ID + MaxNumOfClauses + MaxLenOfClauses, data=dd, mean, na.action=na.pass) # discard iterations
+dd <- dd[with(dd, order(ID, MaxNumOfClauses, MaxLenOfClauses)), ] # use same order as original data
+dd$Iteration <- NULL # remove iteration column
+
+system_ids <- function(systemPrefix) {
+  return(with(systems, ID[which(startsWith(System, systemPrefix))]))
+}
+mean_matrix <- function(systemPrefix, column) {
+  means = rowSums(matrix(column[dd$ID %in% system_ids(systemPrefix)], nrow = length(max_len) * length(max_num)))
+  matrix = matrix(means, nrow = length(max_num), ncol = length(max_len), byrow = TRUE)
+  rownames(matrix) <- max_num
+  colnames(matrix) <- max_len
+  return(matrix)
+}
+img <- function(main, matrix) {
+  #image(t(m)[,nrow(m):1])
+  par(mar=c(5.1, 4.1, 4.1, 8))
+  plot(matrix, asp = TRUE, col=viridis, digits=0, text.cell=list(cex=0.6), main = main)
 }
 
-#with(dataset, data.frame(num = MaxNumOfClauses[order(SatTime)], len = MaxLenOfClauses[order(SatTime)]))
-
-#mat = with(evaluation, matrix(Variables[ID == 178], nrow = 6, ncol = 9, byrow = TRUE))
-
-mat = with(evaluation, matrix(AnalysisTime[System == "axtls/release-1.0.0.kconfigreader.model"], nrow = 5, ncol = 5, byrow = TRUE))
-#mat = with(evaluation, matrix(Variables[System == "axtls/release-1.0.0.kconfigreader.model"], nrow = 5, ncol = 5, byrow = TRUE))
-#mat = with(evaluation, matrix(Clauses[System == "axtls/release-1.0.0.kconfigreader.model"], nrow = 5, ncol = 5, byrow = TRUE))
-#mat = with(evaluation, matrix(TseytinClauses[System == "axtls/release-1.0.0.kconfigreader.model"], nrow = 5, ncol = 5, byrow = TRUE))
-
-#mat = with(dataset, matrix(AnalysisTime[System == "buildroot/2009.11.kconfigreader.model"], nrow = 5, ncol = 5, byrow = TRUE))
-image(mat)
+systemPrefix = "linux"
+#systemPrefix = "busybox/1_34_0.kconfigreader.model"
+#systemPrefix = "busybox/1_16_1.kconfigreader.model"
+img("TransformTime", mean_matrix(systemPrefix, dd$TransformTime))
+img("Variables", mean_matrix(systemPrefix, dd$Variables))
+img("Clauses", mean_matrix(systemPrefix, dd$Clauses))
+img("TseytinClauses", mean_matrix(systemPrefix, dd$TseytinClauses))
+img("TseytinConstraints", mean_matrix(systemPrefix, dd$TseytinConstraints))
+img("SatTime", mean_matrix(systemPrefix, dd$SatTime))
