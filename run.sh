9ac66358d0c5393e46fc938b924a9faec813b735
Elias Kuiter <info@elias-kuiter.de>
2021-10-05 12:24:27 +0200

    Update

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
 #! /bin/bash
+
+# to use Z3 for Tseytin transformation, you must install Z3 AND its Java bindings:
+# git clone https://github.com/Z3Prover/z3
+# cd z3
+# python scripts/mk_make.py --java
+# cd build
+# make
+# sudo make install
+
 set -e
 mvn install
 rm -f evaluation-tseytin.jar
