9c0cc4804019fc4d135d11e7e7aa0042573f89c6
Elias Kuiter <info@elias-kuiter.de>
2021-10-07 10:57:04 +0200

    Updated R script

--- config/evaluation.R
+++ config/evaluation.R
@@ -9,7 +9,7 @@ replace_na <- function(data, replacement) {
 }
 
 max_num = list(0,1,10,100,1000,10000,100000,2147483647)
-max_len = list(0,1,2,4,8,16,32,64,128,256,512,1024,2147483647)
+max_len = list(0,1,2,4,8,16,32,64,128,2147483647)
 aggregator = rowSums # rowSums or rowMeans
 systems <- read_delim("systems.csv", delim = ";", escape_double = FALSE, trim_ws = TRUE) # read systems
 dd <- read_delim("evaluation.csv", delim = ";", escape_double = FALSE, trim_ws = TRUE) # read actual data
@@ -36,12 +36,16 @@ img <- function(main, matrix) {
   plot(matrix, asp = TRUE, col=viridis, digits=0, text.cell=list(cex=0.6), main = main)
 }
 
-systemPrefix = ""
-#systemPrefix = "busybox/1_34_0.kconfigreader.model"
-#systemPrefix = "busybox/1_16_1.kconfigreader.model"
-img("TransformTime", mean_matrix(systemPrefix, dd$TransformTime))
-img("Variables", mean_matrix(systemPrefix, dd$Variables))
-img("Clauses", mean_matrix(systemPrefix, dd$Clauses))
-img("TseytinClauses", mean_matrix(systemPrefix, dd$TseytinClauses))
-img("TseytinConstraints", mean_matrix(systemPrefix, dd$TseytinConstraints))
-img("SatTime", mean_matrix(systemPrefix, dd$SatTime))
+system = ""
+#system = "busybox/1_34_0.kconfigreader.model"
+#system = "busybox/1_16_1.kconfigreader.model"
+img("TransformTime", mean_matrix(system, dd$TransformTime))
+img("Variables", mean_matrix(system, dd$Variables))
+img("Clauses", mean_matrix(system, dd$Clauses))
+img("TseytinClauses", mean_matrix(system, dd$TseytinClauses))
+img("TseytinConstraints", mean_matrix(system, dd$TseytinConstraints))
+img("SatTime", mean_matrix(system, dd$SatTime))
+
+for (system in c("axtls", "buildroot", "busybox", "embtoolkit", "fiasco", "freetz-ng", "linux", "uclibc-ng")) {
+  img(system, mean_matrix(system, dd$SatTime))
+}
