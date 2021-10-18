22bd0fabdcebd5e7fae3994164e80d6a16068f5f
Elias Kuiter <info@elias-kuiter.de>
2021-10-18 09:46:01 +0200

    Major refactoring of evaluation

---
+++ src/main/java/org/spldev/evaluation/tseytin/EvaluationRunner.java
@@ -0,0 +1,362 @@
+/* -----------------------------------------------------------------------------
+ * Evaluation-Tseytin - Program for the evaluation of the Tseytin transformation.
+ * Copyright (C) 2021  Sebastian Krieter, Elias Kuiter
+ *
+ * This file is part of Evaluation-Tseytin.
+ *
+ * Evaluation-Tseytin is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * Evaluation-Tseytin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Evaluation-Tseytin.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * See <https://github.com/ekuiter/evaluation-tseytin> for further information.
+ * -----------------------------------------------------------------------------
+ */
+package org.spldev.evaluation.tseytin;
+
+import de.ovgu.featureide.fm.core.analysis.cnf.CNF;
+import de.ovgu.featureide.fm.core.analysis.cnf.LiteralSet;
+import de.ovgu.featureide.fm.core.analysis.cnf.formula.FeatureModelFormula;
+import de.ovgu.featureide.fm.core.base.IFeatureModel;
+import de.ovgu.featureide.fm.core.init.FMCoreLibrary;
+import de.ovgu.featureide.fm.core.init.LibraryManager;
+import de.ovgu.featureide.fm.core.io.manager.FeatureModelManager;
+import de.ovgu.featureide.fm.core.job.monitor.NullMonitor;
+import org.prop4j.Node;
+import org.spldev.evaluation.util.ModelReader;
+import org.spldev.formula.ModelRepresentation;
+import org.spldev.formula.analysis.sat4j.AtomicSetAnalysis;
+import org.spldev.formula.analysis.sat4j.CoreDeadAnalysis;
+import org.spldev.formula.analysis.sat4j.HasSolutionAnalysis;
+import org.spldev.formula.analysis.sharpsat.CountSolutionsAnalysis;
+import org.spldev.formula.clauses.LiteralList;
+import org.spldev.formula.expression.Formula;
+import org.spldev.formula.expression.atomic.literal.VariableMap;
+import org.spldev.formula.expression.io.DIMACSFormat;
+import org.spldev.formula.expression.io.FormulaFormatManager;
+import org.spldev.formula.expression.io.parse.NodeReader;
+import org.spldev.formula.expression.io.parse.NodeWriter;
+import org.spldev.formula.expression.transform.CNFTransformer;
+import org.spldev.formula.expression.transform.NormalForms;
+import org.spldev.formula.expression.transform.Transformer;
+import org.spldev.formula.solver.javasmt.CNFTseytinTransformer;
+import org.spldev.util.data.Pair;
+import org.spldev.util.extension.ExtensionLoader;
+import org.spldev.util.io.FileHandler;
+import org.spldev.util.job.Executor;
+import org.spldev.util.logging.Logger;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class EvaluationRunner {
+	private static EvaluationWrapper.Parameters parameters;
+
+	private static class Result<T> extends Pair<Long, T> {
+		public Result(Long timeNeeded, T result) {
+			super(timeNeeded, result);
+		}
+	}
+
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			throw new RuntimeException("invalid usage");
+		}
+		ExtensionLoader.load();
+		parameters = EvaluationWrapper.Parameters.read(Paths.get(args[0]));
+		Objects.requireNonNull(parameters);
+		System.out.println(parameters);
+
+		if (parameters.analysisAlgorithm == EvaluationWrapper.AnalysisAlgorithm.TRANSFORM) {
+			if (parameters.transformationAlgorithm == EvaluationWrapper.TransformationAlgorithm.TSEYTIN_Z3) {
+				Formula formula = readFormula(Paths.get(parameters.modelPath));
+				processFormulaResult(executeTransformer(formula, new CNFTseytinTransformer()));
+			} else if (parameters.transformationAlgorithm == EvaluationWrapper.TransformationAlgorithm.TSEYTIN_SPLDEV) {
+				Formula formula = readFormula(Paths.get(parameters.modelPath));
+				CNFTransformer transformer = new CNFTransformer();
+				transformer.setMaximumNumberOfClauses(0);
+				transformer.setMaximumLengthOfClauses(0);
+				transformer.setMaximumNumberOfLiterals(0);
+				processFormulaResult(executeTransformer(formula, transformer));
+			} else if (parameters.transformationAlgorithm == EvaluationWrapper.TransformationAlgorithm.DISTRIB_FEATUREIDE) {
+				// this throws for large models (stack overflow) ... maybe save as xml and load
+				// again?
+				Formula formula = readFormula(Paths.get(parameters.modelPath));
+				LibraryManager.registerLibrary(FMCoreLibrary.getInstance());
+				NodeWriter spldevNodeWriter = new NodeWriter();
+				Function<Node, org.prop4j.NodeWriter> featureideNodeWriter = org.prop4j.NodeWriter::new;
+				NodeReader spldevNodeReader = new NodeReader();
+				org.prop4j.NodeReader featureideNodeReader = new org.prop4j.NodeReader();
+				featureideNodeReader.activateShortSymbols();
+				String serializedFormula = spldevNodeWriter.write(formula);
+				Node node = featureideNodeReader.stringToNode(serializedFormula);
+				if (node == null)
+					throw new RuntimeException("formula could not be read by FeatureIDE");
+				Result<Node> result = execute(() -> {
+					final long localTime = System.nanoTime();
+					Node cnf = node.toRegularCNF();
+					final long timeNeeded = System.nanoTime() - localTime;
+					return new Result<>(timeNeeded, cnf);
+				});
+				if (result != null) {
+					serializedFormula = featureideNodeWriter.apply(result.getValue()).nodeToString();
+					if (serializedFormula == null)
+						throw new RuntimeException("formula could not be written by FeatureIDE");
+					org.spldev.util.Result<Formula> protoCnfFormula = spldevNodeReader.read(serializedFormula);
+					if (!protoCnfFormula.isPresent())
+						throw new RuntimeException("formula could not be read by spldev");
+					Formula cnfFormula = NormalForms.toClausalNF(NormalForms.simplifyForNF(protoCnfFormula.get()),
+						NormalForms.NormalForm.CNF);
+					processFormulaResult(new Result<>(result.getKey(), cnfFormula));
+				}
+			} else if (parameters.transformationAlgorithm == EvaluationWrapper.TransformationAlgorithm.DISTRIB_SPLDEV) {
+				Formula formula = readFormula(Paths.get(parameters.modelPath));
+				CNFTransformer transformer = new CNFTransformer();
+				transformer.setMaximumNumberOfClauses(Integer.MAX_VALUE);
+				transformer.setMaximumLengthOfClauses(Integer.MAX_VALUE);
+				transformer.setMaximumNumberOfLiterals(Integer.MAX_VALUE);
+				processFormulaResult(executeTransformer(formula, transformer));
+			} else {
+				throw new RuntimeException("invalid algorithm");
+			}
+		} else if (parameters.analysisAlgorithm == EvaluationWrapper.AnalysisAlgorithm.SAT_FEATUREIDE) {
+			LibraryManager.registerLibrary(FMCoreLibrary.getInstance());
+			final IFeatureModel featureModel = FeatureModelManager.load(getTempPath());
+			if (featureModel != null) {
+				CNF cnf = new FeatureModelFormula(featureModel).getCNF(); // todo: can this be done faster?
+				printResult(execute(() -> {
+					final long localTime = System.nanoTime();
+					Boolean sat = null;
+					try {
+						sat = new de.ovgu.featureide.fm.core.analysis.cnf.analysis.HasSolutionAnalysis(cnf)
+							.analyze(new NullMonitor<>());
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					final long timeNeeded = System.nanoTime() - localTime;
+					return new Result<>(timeNeeded, sat);
+				}));
+			}
+		} else if (parameters.analysisAlgorithm == EvaluationWrapper.AnalysisAlgorithm.SAT_SPLDEV) {
+			final org.spldev.util.Result<ModelRepresentation> rep = ModelRepresentation.load(getTempPath());
+			final HasSolutionAnalysis hasSolutionAnalysis = new HasSolutionAnalysis();
+			if (rep.isPresent()) {
+				printResult(execute(() -> {
+					final long localTime = System.nanoTime();
+					final Boolean sat = hasSolutionAnalysis.getResult(rep.get()).orElseThrow();
+					final long timeNeeded = System.nanoTime() - localTime;
+					return new Result<>(timeNeeded, sat);
+				}));
+			}
+		} else if (parameters.analysisAlgorithm == EvaluationWrapper.AnalysisAlgorithm.CORE_DEAD_FEATUREIDE) {
+			LibraryManager.registerLibrary(FMCoreLibrary.getInstance());
+			final IFeatureModel featureModel = FeatureModelManager.load(getTempPath());
+			if (featureModel != null) {
+				CNF cnf = new FeatureModelFormula(featureModel).getCNF(); // todo: can this be done faster?
+				printResult(execute(() -> {
+					final long localTime = System.nanoTime();
+					LiteralSet coreDead = null;
+					try {
+						coreDead = new de.ovgu.featureide.fm.core.analysis.cnf.analysis.CoreDeadAnalysis(cnf)
+							.analyze(new NullMonitor<>());
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					final long timeNeeded = System.nanoTime() - localTime;
+					if (coreDead == null)
+						return null;
+					coreDead = coreDead.retainAll(cnf.getVariables().convertToLiterals(getNonTseytinFeatures(cnf), true,
+						true));
+					return new Result<>(timeNeeded, coreDead.size());
+				}));
+			}
+		} else if (parameters.analysisAlgorithm == EvaluationWrapper.AnalysisAlgorithm.CORE_DEAD_SPLDEV) {
+			final org.spldev.util.Result<ModelRepresentation> rep = ModelRepresentation.load(getTempPath());
+			final CoreDeadAnalysis coreDeadAnalysis = new CoreDeadAnalysis();
+			// todo: we can calculate this for ALL features, or only the non-Tseytin
+			// features. But it should be comparable
+			// to the atomic sets! (is there any difference anyway?)
+			// coreDeadAnalysis.setVariables(LiteralList.getVariables(rep.getVariables(),
+			// getNotTseytinFeatures(rep)));
+			if (rep.isPresent()) {
+				printResult(execute(() -> {
+					final long localTime = System.nanoTime();
+					LiteralList coreDead = coreDeadAnalysis.getResult(rep.get()).orElseThrow();
+					final long timeNeeded = System.nanoTime() - localTime;
+					coreDead = coreDead.retainAll(LiteralList.getLiterals(rep.get().getVariables(),
+						getNonTseytinFeatures(rep.get())));
+					return new Result<>(timeNeeded, coreDead.size());
+				}));
+			}
+		} else if (parameters.analysisAlgorithm == EvaluationWrapper.AnalysisAlgorithm.ATOMIC_SET_FEATUREIDE) {
+			LibraryManager.registerLibrary(FMCoreLibrary.getInstance());
+			final IFeatureModel featureModel = FeatureModelManager.load(getTempPath());
+			if (featureModel != null) {
+				CNF cnf = new FeatureModelFormula(featureModel).getCNF(); // todo: can this be done faster?
+				printResult(execute(() -> {
+					final long localTime = System.nanoTime();
+					List<LiteralSet> atomicSets = null;
+					try {
+						atomicSets = new de.ovgu.featureide.fm.core.analysis.cnf.analysis.AtomicSetAnalysis(cnf)
+							.analyze(new NullMonitor<>());
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					final long timeNeeded = System.nanoTime() - localTime;
+					if (atomicSets == null)
+						return null;
+					LiteralSet nonTseytinFeatures = cnf.getVariables().convertToLiterals(getNonTseytinFeatures(cnf),
+						true, true);
+					atomicSets = atomicSets.stream()
+						.map(atomicSet -> atomicSet.retainAll(nonTseytinFeatures))
+						.filter(atomicSet -> !atomicSet.isEmpty())
+						.collect(Collectors.toList());
+					return new Result<>(timeNeeded, atomicSets.size());
+				}));
+			}
+		} else if (parameters.analysisAlgorithm == EvaluationWrapper.AnalysisAlgorithm.ATOMIC_SET_SPLDEV) {
+			final org.spldev.util.Result<ModelRepresentation> rep = ModelRepresentation.load(getTempPath());
+			final AtomicSetAnalysis atomicSetAnalysis = new AtomicSetAnalysis();
+			// todo: maybe only calculate this for non-tseytin variables?
+			if (rep.isPresent()) {
+				printResult(execute(() -> {
+					final long localTime = System.nanoTime();
+					List<LiteralList> atomicSets = atomicSetAnalysis.getResult(rep.get()).orElseThrow();
+					final long timeNeeded = System.nanoTime() - localTime;
+					LiteralList nonTseytinFeatures = LiteralList.getLiterals(rep.get().getVariables(),
+						getNonTseytinFeatures(rep.get()));
+					atomicSets = atomicSets.stream()
+						.map(atomicSet -> atomicSet.retainAll(nonTseytinFeatures))
+						.filter(atomicSet -> !atomicSet.isEmpty())
+						.collect(Collectors.toList());
+					return new Result<>(timeNeeded, atomicSets.size());
+				}));
+			}
+		} else if (parameters.analysisAlgorithm == EvaluationWrapper.AnalysisAlgorithm.SHARP_SAT) {
+			final org.spldev.util.Result<ModelRepresentation> rep = ModelRepresentation.load(getTempPath());
+			final CountSolutionsAnalysis countSolutionsAnalysis = new CountSolutionsAnalysis();
+			countSolutionsAnalysis.setTimeout((int) (Math.ceil(parameters.timeout) / 1000.0));
+			if (rep.isPresent()) {
+				printResult(execute(() -> {
+					final long localTime = System.nanoTime();
+					final BigInteger sharpSat = countSolutionsAnalysis.getResult(rep.get()).get();
+					final long timeNeeded = System.nanoTime() - localTime;
+					return new Result<>(timeNeeded, sharpSat);
+				}));
+			}
+		} else {
+			throw new RuntimeException("invalid algorithm");
+		}
+	}
+
+	private static List<String> getNonTseytinFeatures(ModelRepresentation rep) {
+		return rep.getVariables().getNames().stream()
+			.filter(name -> name != null && !name.startsWith("__temp__"))
+			.filter(name -> !name.startsWith("k!"))
+			.collect(Collectors.toList());
+	}
+
+	private static List<String> getNonTseytinFeatures(CNF cnf) {
+		return Arrays.stream(cnf.getVariables().getNames())
+			.filter(name -> name != null && !name.startsWith("__temp__"))
+			.filter(name -> !name.startsWith("k!"))
+			.collect(Collectors.toList());
+	}
+
+	// todo: indeterminate analysis is currently not implemented correctly, AFAIK
+//    private static Result<Integer> indeterminate(ModelRepresentation rep) {
+//        final IndeterminateAnalysis indeterminateAnalysis = new IndeterminateAnalysis();
+//        final long localTime = System.nanoTime();
+//        LiteralList indeterminate = indeterminateAnalysis.getResult(rep).orElseThrow();
+//        final long timeNeeded = System.nanoTime() - localTime;
+//        System.err.printf("got %d/%d indeterminate Tseytin features%n",
+//                indeterminate.retainAll(LiteralList.getLiterals(rep.getVariables(), getTseytinFeatures(rep))).size(),
+//                getTseytinFeatures(rep).size());
+//        indeterminate = indeterminate.retainAll(LiteralList.getLiterals(rep.getVariables(), getNotTseytinFeatures(
+//                rep)));
+//        return new Result<>(indeterminate.size(), timeNeeded);
+//    }
+
+	private static <T> T execute(Callable<T> method) {
+		final ExecutorService executor = Executors.newSingleThreadExecutor();
+		final Future<T> future = executor.submit(method);
+		try {
+			return future.get(parameters.timeout, TimeUnit.MILLISECONDS);
+		} catch (final TimeoutException e) {
+			System.exit(0);
+		} catch (ExecutionException | InterruptedException e) {
+			e.printStackTrace();
+			System.exit(0);
+		} finally {
+			executor.shutdownNow();
+		}
+		return null;
+	}
+
+	private static Result<Formula> executeTransformer(Formula formula, Transformer transformer) {
+		return execute(() -> {
+			final long localTime = System.nanoTime();
+			Formula transformedFormula = Executor.run(transformer, formula).orElse(Logger::logProblems);
+			final long timeNeeded = System.nanoTime() - localTime;
+			return new Result<>(timeNeeded, transformedFormula);
+		});
+	}
+
+	private static Formula readFormula(Path path) {
+		final ModelReader<Formula> fmReader = new ModelReader<>();
+		fmReader.setPathToFiles(Paths.get(parameters.rootPath));
+		fmReader.setFormatSupplier(FormulaFormatManager.getInstance());
+		return fmReader.read(path.toString()).orElseThrow(p -> new RuntimeException("no feature model"));
+	}
+
+	private static void writeFormula(Formula formula, Path path) {
+		try {
+			FileHandler.save(formula, path, new DIMACSFormat());
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static Path getTempPath() {
+		return Paths.get(parameters.tempPath).resolve(
+			String.format("%s_%s_%d.dimacs",
+				parameters.system.replaceAll("[./]", "_"),
+				parameters.transformationAlgorithm, parameters.iteration));
+	}
+
+	static void printResult(Object o) {
+		System.out.println(EvaluationWrapper.RESULT_PREFIX + o);
+	}
+
+	private static void processFormulaResult(Result<Formula> result) {
+		if (result != null) {
+			printResult(result.getKey());
+			printResult(VariableMap.fromExpression(result.getValue()).size());
+			printResult(result.getValue().getChildren().size());
+			writeFormula(result.getValue(), getTempPath());
+		}
+	}
+
+	private static void printResult(Result<?> result) {
+		if (result != null) {
+			printResult(result.getKey());
+			printResult(result.getValue());
+		}
+	}
+}
--- src/main/java/org/spldev/evaluation/tseytin/TseytinEvaluator.java
+++ src/main/java/org/spldev/evaluation/tseytin/TseytinEvaluator.java
@@ -1,29 +1,29 @@
 /* -----------------------------------------------------------------------------
  * Evaluation-Tseytin - Program for the evaluation of the Tseytin transformation.
  * Copyright (C) 2021  Sebastian Krieter, Elias Kuiter
- * 
+ *
  * This file is part of Evaluation-Tseytin.
- * 
+ *
  * Evaluation-Tseytin is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
- * 
+ *
  * Evaluation-Tseytin is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Evaluation-Tseytin.  If not, see <https://www.gnu.org/licenses/>.
- * 
+ *
  * See <https://github.com/ekuiter/evaluation-tseytin> for further information.
  * -----------------------------------------------------------------------------
  */
 package org.spldev.evaluation.tseytin;
 
 import java.util.*;
-import java.util.stream.*;
+import java.util.stream.Collectors;
 
 import org.spldev.evaluation.*;
 import org.spldev.evaluation.process.*;
@@ -34,29 +34,13 @@ import org.spldev.formula.expression.atomic.literal.*;
 import org.spldev.formula.expression.io.*;
 import org.spldev.formula.expression.transform.*;
 import org.spldev.util.io.csv.*;
-import org.spldev.util.logging.*;
 
-/**
- * Evaluate the (hybrid) Tseytin transformation. This assumes that input
- * formulas are in a "partial" CNF (i.e., already a conjunction of some
- * formulas).
- */
 public class TseytinEvaluator extends Evaluator {
-
-	protected static final ListProperty<Integer> maxLiteralsProperty = new ListProperty<>("maxLiterals",
-		Property.IntegerConverter);
 	protected static final ListProperty<String> skipSharpSatProperty = new ListProperty<>("skipSharpSat",
-		Property.StringConverter);
-	protected static final Property<String> transformerProperty = new Property<>("transformer",
 		Property.StringConverter);
 
 	protected CSVWriter writer, systemWriter;
 	protected ProcessRunner processRunner;
-	private String systemName;
-	private int maxLiterals;
-	private Formula formula;
-	private final String[] results = new String[13];
-	private int algorithmIteration;
 
 	@Override
 	public String getName() {
@@ -71,102 +55,93 @@ public class TseytinEvaluator extends Evaluator {
 	@Override
 	protected void addCSVWriters() {
 		super.addCSVWriters();
-		writer = addCSVWriter("evaluation.csv",
-			Arrays.asList("ID", "MaxLiterals", "Iteration",
-				"TransformTime", "Variables", "Clauses", "TseytinClauses", "TseytinConstraints",
-				"SatTime", "Sat", "CoreTime", "Core", "AtomicTime", "Atomic",
-				"SharpSatTime", "SharpSat"));
+		List<String> resultColumns = new ArrayList<>();
+		resultColumns.add("ID");
+		resultColumns.add("Iteration");
+		resultColumns.add("Algorithm");
+		EvaluationWrapper.algorithmResultColumns.forEach(algorithmPair -> resultColumns.addAll(Arrays.asList(
+			algorithmPair.getValue())));
+		writer = addCSVWriter("evaluation.csv", resultColumns);
 		systemWriter = addCSVWriter("systems.csv", Arrays.asList("ID", "System", "Features", "Constraints"));
 	}
 
 	@Override
 	public void evaluate() {
 		tabFormatter.setTabLevel(0);
-		final int systemIndexEnd = config.systemNames.size();
 		final ModelReader<Formula> fmReader = new ModelReader<>();
 		fmReader.setPathToFiles(config.modelPath);
 		fmReader.setFormatSupplier(FormulaFormatManager.getInstance());
 		processRunner = new ProcessRunner();
-		processRunner.setTimeout(config.timeout.getValue());
-		final List<Integer> maxLiteralsValues = maxLiteralsProperty.getValue();
-		for (algorithmIteration = 0; algorithmIteration < config.algorithmIterations.getValue(); algorithmIteration++) {
-			for (systemIndex = 0; systemIndex < systemIndexEnd; systemIndex++) {
-				systemName = config.systemNames.get(systemIndex);
-				if (algorithmIteration == 0) {
-					formula = fmReader.read(systemName).orElseThrow(p -> new RuntimeException("no feature model"));
-					writeCSV(systemWriter, this::writeSystem);
+		processRunner.setTimeout(config.timeout.getValue() * 2);
+		for (systemIteration = 0; systemIteration < config.systemIterations.getValue(); systemIteration++) {
+			for (systemIndex = 0; systemIndex < config.systemNames.size(); systemIndex++) {
+				String modelPath = config.systemNames.get(systemIndex);
+				String system = modelPath.replace(".kconfigreader.model", "");
+				if (systemIteration == 0) {
+					Formula formula = fmReader.read(modelPath).orElseThrow(p -> new RuntimeException(
+						"no feature model"));
+					writeCSV(systemWriter, systemWriter -> {
+						systemWriter.addValue(systemIndex);
+						systemWriter.addValue(system);
+						systemWriter.addValue(VariableMap.fromExpression(formula).size());
+						systemWriter.addValue(NormalForms.simplifyForNF(formula).getChildren().size());
+					});
 				}
-				for (systemIteration = 0; systemIteration < config.systemIterations.getValue(); systemIteration++) {
-					tabFormatter.setTabLevel(0);
-					logSystem();
-					tabFormatter.setTabLevel(1);
-					long lastMaxLiterals = Long.MAX_VALUE;
-
-					for (Integer maxLiteralsValue : maxLiteralsValues) {
-						maxLiterals = maxLiteralsValue;
-						if (maxLiterals >= lastMaxLiterals) {
-							Logger.logInfo("Skipping for " + systemName + " " + maxLiterals);
-						} else {
-							transform();
-							if ("NA".equals(results[0]) || "0".equals(results[4])) {
-								lastMaxLiterals = maxLiterals;
-							}
-							writeCSV(writer, this::writeResults);
-						}
-					}
-				}
+				EvaluationWrapper.Parameters parameters = new EvaluationWrapper.Parameters(
+					system, config.modelPath.toString(),
+					modelPath, systemIteration, config.tempPath.toString(), config.timeout.getValue());
+				tabFormatter.setTabLevel(0);
+				logSystem();
+				tabFormatter.setTabLevel(1);
+				Arrays.stream(EvaluationWrapper.transformationAlgorithms).forEach(transformationAlgorithm -> {
+					List<String> results = evaluateForParameters(parameters, transformationAlgorithm);
+					writeCSV(writer, writer -> {
+						writer.addValue(systemIndex);
+						writer.addValue(systemIteration);
+						writer.addValue(transformationAlgorithm);
+						results.forEach(writer::addValue);
+					});
+				});
 			}
 		}
 	}
 
-	private void transform() {
-		Logger.logInfo("Running for " + systemName + " " + maxLiterals);
+	private List<String> evaluateForParameters(EvaluationWrapper.Parameters parameters,
+		EvaluationWrapper.TransformationAlgorithm transformationAlgorithm) {
 		tabFormatter.setTabLevel(2);
-		final TseytinAlgorithm algorithm = new TseytinAlgorithm(config.modelPath, systemName,
-			maxLiterals, algorithmIteration + "_" + systemIteration, config.tempPath, config.timeout.getValue(),
-			transformerProperty.getValue());
-		Arrays.fill(results, "NA");
-		runAlgorithm(algorithm, "model2cnf", 0);
-		runAlgorithm(algorithm, "sat", 5);
-		runAlgorithm(algorithm, "core", 7);
-		runAlgorithm(algorithm, "atomic", 9);
-		// runAlgorithm(algorithm, "indeterminate", 11);
-		if (skipSharpSatProperty.getValue().stream().noneMatch(s -> systemName.startsWith(s)))
-			runAlgorithm(algorithm, "sharpsat", 11);
+		List<String> results = new ArrayList<>();
+		final EvaluationWrapper evaluationWrapper = new EvaluationWrapper(parameters);
+		results.addAll(run(evaluationWrapper, transformationAlgorithm, EvaluationWrapper.AnalysisAlgorithm.TRANSFORM));
+		results.addAll(run(evaluationWrapper, transformationAlgorithm,
+			EvaluationWrapper.AnalysisAlgorithm.SAT_FEATUREIDE));
+		results.addAll(run(evaluationWrapper, transformationAlgorithm, EvaluationWrapper.AnalysisAlgorithm.SAT_SPLDEV));
+		results.addAll(run(evaluationWrapper, transformationAlgorithm,
+			EvaluationWrapper.AnalysisAlgorithm.CORE_DEAD_FEATUREIDE));
+		results.addAll(run(evaluationWrapper, transformationAlgorithm,
+			EvaluationWrapper.AnalysisAlgorithm.CORE_DEAD_SPLDEV));
+		results.addAll(run(evaluationWrapper, transformationAlgorithm,
+			EvaluationWrapper.AnalysisAlgorithm.ATOMIC_SET_FEATUREIDE));
+		results.addAll(run(evaluationWrapper, transformationAlgorithm,
+			EvaluationWrapper.AnalysisAlgorithm.ATOMIC_SET_SPLDEV));
+		if (skipSharpSatProperty.getValue().stream().noneMatch(s -> parameters.modelPath.startsWith(s)))
+			results.addAll(run(evaluationWrapper, transformationAlgorithm,
+				EvaluationWrapper.AnalysisAlgorithm.SHARP_SAT));
+		else
+			results.addAll(Arrays.stream(EvaluationWrapper.AnalysisAlgorithm.SHARP_SAT.getResultColumns())
+				.map(c -> "NA").collect(Collectors.toList()));
+		return results;
 	}
 
-	private void runAlgorithm(final TseytinAlgorithm algorithm, final String name, int resultIndex) {
-		Logger.logInfo(name);
-		algorithm.setStage(name);
+	private List<String> run(EvaluationWrapper evaluationWrapper,
+		EvaluationWrapper.TransformationAlgorithm transformationAlgorithm,
+		EvaluationWrapper.AnalysisAlgorithm analysisAlgorithm) {
+		evaluationWrapper.setTransformationAlgorithm(transformationAlgorithm);
+		evaluationWrapper.setAnalysisAlgorithm(analysisAlgorithm);
 		tabFormatter.incTabLevel();
-		copyResults(processRunner.run(algorithm).getResult(), resultIndex);
+		List<String> results = processRunner.run(evaluationWrapper).getResult();
+		while (results.size() < analysisAlgorithm.getResultColumns().length)
+			results.add("NA");
 		tabFormatter.decTabLevel();
+		return results;
 	}
-
-	private void writeResults(CSVWriter writer) {
-		writer.addValue(systemIndex);
-		writer.addValue(maxLiterals);
-		writer.addValue(algorithmIteration + "_" + systemIteration);
-		for (final String value : results) {
-			writer.addValue(value);
-		}
-	}
-
-	private void writeSystem(CSVWriter systemWriter) {
-		systemWriter.addValue(systemIndex);
-		systemWriter.addValue(systemName);
-		systemWriter.addValue(VariableMap.fromExpression(formula).size());
-		systemWriter.addValue(NormalForms.simplifyForNF(formula).getChildren().size());
-	}
-
-	private void copyResults(List<String> resultList, int start) {
-		resultList = resultList.stream()
-			.filter(line -> line.startsWith("res="))
-			.map(line -> line.replace("res=", ""))
-			.collect(Collectors.toList());
-		for (final String value : resultList) {
-			results[start++] = value;
-		}
-	}
-
 }
