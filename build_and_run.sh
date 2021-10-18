e022d3cbe325971e95b347c40eba43f4c314501d
Elias Kuiter <info@elias-kuiter.de>
2021-10-18 09:47:13 +0200

    Update

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+
+# { cd ~/dbse/feature-model-repository/models; find . } | cut -d/ -f2- | grep '.kconfigreader.model\|.xml' | sort -V > config/models.txt
+
 set -e
 bash build.sh
 bash run.sh
